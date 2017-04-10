9dd121bd67b69b4a6b36106b5254da59d44f354e
Mayank Joshi <mayank@aisle.co>
2017-04-10 23:44:38 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+var bodyParser = require('body-parser')
+var express = require('express');
+const _ = require('lodash');
+
+const publicPath = path.join(__dirname,'/../public');
+console.log(__dirname + '/../public');
+console.log(publicPath);
+
+var app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(express.static(publicPath));
+
+// Get /1
+// app.get('/1',(req, res) => {
+//     return res.send('<p>dnkbldf</p>');
+// });
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}....`);
+});
