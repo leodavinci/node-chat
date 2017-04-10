a8d34410d5e4e8b27ce11a99881acd9e63c08d88
Mayank Joshi <mayank@aisle.co>
2017-04-10 23:59:33 +0530

    making it heroku friendly

--- server/server.js
+++ server/server.js
@@ -4,11 +4,10 @@ var express = require('express');
 const _ = require('lodash');
 
 const publicPath = path.join(__dirname,'/../public');
-console.log(__dirname + '/../public');
 console.log(publicPath);
 
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
